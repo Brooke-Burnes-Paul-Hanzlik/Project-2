9ee52baa2432067a6bc91940155e21bc4692a915
burne188 <brookeburnes@gmail.com>
2014-10-13 21:23:18 -0500

    initial

---
+++ BattleShipBoard.java
@@ -0,0 +1,22 @@
+
+public class BattleShipBoard {
+	
+	public static void BattleShipBoard(int m, int n) {
+		//instantiates a 2-dimensional array with dimensions m x n
+		//if(m>3 && m<10 && n>3 && n<10){
+			int [][] Board = new int[m][n];
+		//}
+		
+		if (m<=4 && n<=4) {
+			Board[Math.floor()][Math.random()];
+		}
+		
+		if (m>4 && m<=6 && n>4 && m<=6) {
+			//hide 2 ships
+		}
+		
+		if (m>6 && n>6) {
+			//hide 3 ships
+		}
+	}
+}
